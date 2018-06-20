cbed5b3fc360440801d42dde1ead6db8989efe92
Kristopher Kick <kkick@mybluetech.com>
2018-06-20 18:30:38 -0500

    add password hashing with bcryptjs

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 4
-};
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var password = '123abc!';
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded: ', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$Hd4aZlgWy2ZX2zVUenHzcuMcU5tfRbIvqirRjfOMUfrQOWURxhgk.';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 4
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded: ', decoded);
 
 // var message = 'Hello world';
 // var hash = SHA256(message).toString();
